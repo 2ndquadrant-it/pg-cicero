e1d6cc38a8430e407a0dcaa83e1f572ef6119cf9
Carlo Ascani <carlo.ascani@2ndquadrant.it>
2011-05-26 12:29:03 +0200

    added pot files

--- scripts/pgc-migrate.sh
+++ scripts/pgc-migrate.sh
@@ -15,18 +15,19 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 function usage(){
-    echo 
     echo "Usage:"
     echo 
     echo "  $0 [OPTIONS]"
     echo
     echo "Options:"
-    echo " -b BASE_DIR    where original XML files in DocBook format are"
-    echo " -f FROM_DIR    where translated po files are located"
-    echo " -t TO_DIR      where generated .xml files will be placed"
-    echo " -l LANGUAGE    translation language"
+    echo "  -b BASE_DIR    where original XML files in DocBook format are"
+    echo "  -f FROM_DIR    where translated XML files are located"
+    echo "  -o TO_DIR      where translated .po files will be placed"
+    echo "  -l LANGUAGE    translation language"
+    echo "  -p POT_DIR     if specified, it generates untranslated PO files"
     echo
 }
 
@@ -36,19 +37,22 @@ then
     exit 65
 fi
 
-while getopts ":b:f:t:l:p:" opt; do
+while getopts ":b:f:o:l:p:" opt; do
     case $opt in
          b)
              BASEDIR=$OPTARG
              ;;
          f)
-             PODIR=$OPTARG
+             REUSEDIR=$OPTARG
              ;;
-         t)
-             OUTPUTDIR=$OPTARG
+         o)
+             PODIR=$OPTARG
              ;;
          l)
              LANGUAGE=$OPTARG
+             ;;
+         p)
+             POTDIR=$OPTARG
              ;;
          \?)
              echo "Invalid option: -$OPTARG" >&2
@@ -74,15 +78,14 @@ if [ ! -n "$BASEDIR" ]; then
     usage
     exit 1
 fi
+if [ -n "$REUSEDIR" ]; then
+    REUSEOPT=1
+else
+    REUSEOPT=0
+fi
 if [ ! -n "$PODIR" ]; then
     echo 
-    echo "ERROR: you must specify a directory where translated DocBook po files are located (-f option)"
-    usage
-    exit 1
-fi
-if [ ! -n "$OUTPUTDIR" ]; then
-    echo 
-    echo "ERROR: you must specify a destination directory for the generated XML files (-t option) "
+    echo "ERROR: you must specify a destination directory for the generated PD files (-t option) "
     usage
     exit 1
 fi
@@ -124,18 +127,31 @@ i=0
 tot=0
 ok=0
 fail=0
-echo "-> Creating XMLs in "$OUTPUTDIR"/"$LANGUAGE
+echo "-> Creating POs in "$PODIR"/"$LANGUAGE
 START_TIME=`date '+%T' 2>/dev/null`
+
+WORKDIR=`mktemp -d -t cicero-XXXX`
+cp -r $BASEDIR $WORKDIR/base
+if [ $REUSEOPT -eq 1 ]; then
+    cp -r $REUSEDIR $WORKDIR/reuse
+fi
+
 for srcfile in $( find $BASEDIR -type f -name '*.xml' | sed -e "s|$BASEDIR/||" ) 
 do
     INPUT_FILE=$srcfile
-    OUTPUT_FILE=$OUTPUTDIR/$LANGUAGE/$srcfile
-    PO_FILE=$PODIR/$( echo $srcfile | sed -e 's/\.xml/\.po/' )
+    OUTPUT_FILE=$PODIR/$( echo $srcfile | sed -e 's/\.xml/\.po/' )
 
     if [ ! -d `dirname $OUTPUT_FILE` ]; then
         mkdir -p `dirname $OUTPUT_FILE`
     fi
-    xml2po $LANGOPT -e --po-file=$PO_FILE --output=$OUTPUT_FILE $BASEDIR/$INPUT_FILE
+    
+    sed -i -e 's/&mdash;/-/g' $WORKDIR/base/$srcfile
+    if [ $REUSEOPT -eq 1 ]; then
+        sed -i -e 's/&mdash;/-/g' $WORKDIR/reuse/$srcfile 
+        xml2po $LANGOPT --reuse=reuse/$srcfile --output=$OUTPUT_FILE $WORKDIR/base/$srcfile
+    else
+        xml2po $LANGOPT --output=$OUTPUT_FILE $WORKDIR/base/$srcfile
+    fi
 
     if [ $? -eq 0 ]; then
         ok=$ok+1
@@ -144,17 +160,60 @@ do
         fail=$fail+1
     fi
     echo -ne "$(progressbar_step $i)\r"
+
+    msguniq --use-first $OUTPUT_FILE -o $OUTPUT_FILE.unique && mv $OUTPUT_FILE.unique $OUTPUT_FILE 
+
     tot=$tot+1 
     i=$i+1
 done
-END_TIME=`date '+%T' 2>/dev/null`
 
+echo -ne "$(progressbar_step $i)\r"
 echo 
 echo "  Complete!"
 echo "  Total Files : " $tot 
 echo "  Successes   : " $ok
 echo "  Fails       : " $fail
-echo "  Started at  : " $START_TIME
-echo "  Ended at    : " $END_TIME
 echo 
 
+if [ -n "$POTDIR" ]; then
+
+    declare -i tot 
+    declare -i ok
+    declare -i fail
+    declare -i i
+    i=0
+    tot=0
+    ok=0
+    fail=0
+    echo "-> Generating POTs in "$POTDIR
+    for srcfile in $( find $BASEDIR -type f -name '*.xml' | sed -e "s|$BASEDIR/||" ) 
+    do
+        INPUT_FILE=$srcfile
+        OUTPUT_FILE=$POTDIR/$( echo $srcfile | sed -e 's/\.xml/\.po/' )
+    
+        sed -i -e 's/&mdash;/-/g' $WORKDIR/base/$srcfile
+        if [ ! -d `dirname $OUTPUT_FILE` ]; then
+            mkdir -p `dirname $OUTPUT_FILE`
+        fi
+        xml2po -o $OUTPUT_FILE $BASEDIR/$INPUT_FILE
+    
+        if [ $? -eq 0 ]; then
+             ok=$ok+1
+        else
+             echo "ERROR generating " $OUTPUT_FILE
+             fail=$fail+1
+        fi
+        echo -ne "$(progressbar_step $i)\r"
+        tot=$tot+1 
+        i=$i+1
+    done
+
+    echo -ne "$(progressbar_step $i)\r"
+    echo 
+    echo "  Complete!"
+    echo "  Total Files : " $tot 
+    echo "  Successes   : " $ok
+    echo "  Fails       : " $fail
+    echo 
+
+fi
--- scripts/pgc-translate.sh
+++ scripts/pgc-translate.sh
@@ -20,14 +20,13 @@ function usage(){
     echo 
     echo "Usage:"
     echo 
-    echo "  $0 [OPTIONS]"
+    echo -e "\t$0 [OPTIONS]"
     echo
     echo "Options:"
     echo " -b BASE_DIR    where original XML files in DocBook format are"
-    echo " -f FROM_DIR    where translated XML files are located"
-    echo " -t TO_DIR      where generated .po files will be placed"
+    echo " -p FROM_DIR    where translated po files are located"
+    echo " -o OUTPUT_DIR  where generated .xml files will be placed"
     echo " -l LANGUAGE    translation language"
-    echo " -p POT_DIR     if specified, where the regenerated POT files will be placed"
     echo
 }
 
@@ -43,16 +42,13 @@ while getopts ":b:f:t:l:p:" opt; do
              BASEDIR=$OPTARG
              ;;
          f)
-             REUSEDIR=$OPTARG
+             PODIR=$OPTARG
              ;;
          t)
-             PODIR=$OPTARG
+             OUTPUTDIR=$OPTARG
              ;;
          l)
              LANGUAGE=$OPTARG
-             ;;
-         p)
-             POTDIR=$OPTARG
              ;;
          \?)
              echo "Invalid option: -$OPTARG" >&2
@@ -78,15 +74,15 @@ if [ ! -n "$BASEDIR" ]; then
     usage
     exit 1
 fi
-if [ ! -n "$REUSEDIR" ]; then
+if [ ! -n "$PODIR" ]; then
     echo 
-    echo "ERROR: you must specify a directory where translated DocBook xml files are located (-f option)"
+    echo "ERROR: you must specify a directory where translated DocBook po files are located (-f option)"
     usage
     exit 1
 fi
-if [ ! -n "$PODIR" ]; then
+if [ ! -n "$OUTPUTDIR" ]; then
     echo 
-    echo "ERROR: you must specify a destination directory for the generated PD files (-t option) "
+    echo "ERROR: you must specify a destination directory for the generated XML files (-t option) "
     usage
     exit 1
 fi
@@ -128,24 +124,17 @@ i=0
 tot=0
 ok=0
 fail=0
-echo "-> Creating POs in "$PODIR"/"$LANGUAGE
-START_TIME=`date '+%T' 2>/dev/null`
-
-cp -r $BASEDIR base
-cp -r $REUSEDIR reuse
-
+echo "-> Creating XMLs in "$OUTPUTDIR"/"$LANGUAGE
 for srcfile in $( find $BASEDIR -type f -name '*.xml' | sed -e "s|$BASEDIR/||" ) 
 do
     INPUT_FILE=$srcfile
-    OUTPUT_FILE=$PODIR/$LANGUAGE/$( echo $srcfile | sed -e 's/\.xml/\.po/' )
-    #REUSE_FILE=$REUSEDIR/$srcfile
+    OUTPUT_FILE=$OUTPUTDIR/$LANGUAGE/$srcfile
+    PO_FILE=$PODIR/$( echo $srcfile | sed -e 's/\.xml/\.po/' )
 
     if [ ! -d `dirname $OUTPUT_FILE` ]; then
         mkdir -p `dirname $OUTPUT_FILE`
     fi
-    
-    sed -i -e 's/&mdash;/-/g' reuse/$srcfile base/$srcfile
-    xml2po $LANGOPT --reuse=reuse/$srcfile --output=$OUTPUT_FILE base/$srcfile
+    xml2po $LANGOPT --po-file=$PO_FILE --output=$OUTPUT_FILE $BASEDIR/$INPUT_FILE
 
     if [ $? -eq 0 ]; then
         ok=$ok+1
@@ -154,63 +143,15 @@ do
         fail=$fail+1
     fi
     echo -ne "$(progressbar_step $i)\r"
-
-    msguniq --use-first $OUTPUT_FILE -o $OUTPUT_FILE.unique && mv $OUTPUT_FILE.unique $OUTPUT_FILE 
-
     tot=$tot+1 
     i=$i+1
 done
 
+echo -ne "$(progressbar_step $i)\r"
 echo 
 echo "  Complete!"
 echo "  Total Files : " $tot 
 echo "  Successes   : " $ok
 echo "  Fails       : " $fail
-echo "  Started at  : " $START_TIME
-echo "  Ended at    : " $END_TIME
 echo 
 
-if [ -n "$POTDIR" ]; then
-
-    declare -i tot 
-    declare -i ok
-    declare -i fail
-    declare -i i
-    i=0
-    tot=0
-    ok=0
-    fail=0
-    echo "-> Generating POTs in "$POTDIR
-    START_TIME=`date '+%T' 2>/dev/null`
-    for srcfile in $( find $BASEDIR -type f -name '*.xml' | sed -e "s|$BASEDIR/||" ) 
-    do
-        INPUT_FILE=$srcfile
-        OUTPUT_FILE=$POTDIR/$( echo $srcfile | sed -e 's/\.xml/\.pot/' )
-    
-        if [ ! -d `dirname $OUTPUT_FILE` ]; then
-            mkdir -p `dirname $OUTPUT_FILE`
-        fi
-        xml2po -o $OUTPUT_FILE  $BASEDIR/$INPUT_FILE
-    
-        if [ $? -eq 0 ]; then
-             ok=$ok+1
-        else
-             echo "ERROR generating " $OUTPUT_FILE
-             fail=$fail+1
-        fi
-        echo -ne "$(progressbar_step $i)\r"
-        tot=$tot+1 
-        i=$i+1
-    done
-    END_TIME=`date '+%T' 2>/dev/null`
-
-    echo 
-    echo "  Complete!"
-    echo "  Total Files : " $tot 
-    echo "  Successes   : " $ok
-    echo "  Fails       : " $fail
-    echo "  Started at  : " $START_TIME
-    echo "  Ended at    : " $END_TIME
-    echo 
-
-fi
